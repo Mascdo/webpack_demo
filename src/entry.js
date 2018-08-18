a5f0d05466fc0df85482a5339b66d4e465185ced
mascdo <mascdo@outlook.com>
2018-08-18 21:46:51 +0800

    config

---
+++ src/entry.js
@@ -0,0 +1,7 @@
+import css from './css/index.css';
+import less from './css/black.less';
+import sass from './css/bb.scss';
+{
+  let jsString="Hello Webpack!!! Hello Life!!!"
+  document.getElementById("title").innerHTML= jsString;
+}
